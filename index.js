a0a244d7695bf2fede5ef1b4ffdaf6fc5e62e67d
xiaochen <953917730@qq.com>
2022-06-08 11:26:59 +0800

    js-job

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
-var name = 'hello';
-// TODO write your code here...
 
+
+// TODO write your code here...
+var name = 'hello';
+var name1 = name.toUpperCase();
+console.log(name1);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+function titleCase(str){
+    var arr = str.split(' ');
+    for(var i = 0; i<arr.length; i++){
+        arr[i] = arr[i].slice(0,1).toUpperCase() + arr[i].slice(1).toLowerCase();
+    }
+    return arr.join(" ");
+}
+console.log(titleCase(sentence))
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+var res = money.slice(-2)
+console.log(res);
